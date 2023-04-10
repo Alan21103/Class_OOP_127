d1162ea6d85989d9bdc39a516c5125ce008c08b9
Alan21103 <syahlansaputra03@gmail.com>
2023-04-10 10:48:02 +0700

    Membuat Class dan Method

---
+++ OOPClass/OOPClass.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa {
+public:
+		string nim;
+		string nama;
+public: void cetak() {
+	cout << "NIM = " << nim;
+	cout << "\nNama = " << nama;
+}
+};
+
+class Matakuliah {
+private:
+	string kode;
+	string namaM;
+	int sks;
+public:
+	void input() {
+		cout << "Masukan kode Matakuliah : ";
+	}
+}
