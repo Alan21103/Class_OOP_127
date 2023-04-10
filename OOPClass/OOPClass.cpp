5af6f30701a51c05e07f3a3af79c988301ac0dce
Alan21103 <syahlansaputra03@gmail.com>
2023-04-10 11:03:42 +0700

    Membuat Access modifire

--- OOPClass/OOPClass.cpp
+++ OOPClass/OOPClass.cpp
@@ -12,3 +12,37 @@ public: void cetak() {
 };
 
 class Matakuliah {
+private:
+	string kode;
+	string namaM;
+	int sks;
+public:
+	void input() {
+		cout << "Masukan kode Matakuliah : ";
+		cin >> kode;
+		cout << "Masukan Nama Matakuliah : ";
+		cin >> namaM;
+		cout << "Jumlah Sks : ";
+		cin >> sks;
+	}
+	void tampil() {
+		cout << "Kode Matakuliah : ";
+		cout << "Nama Matakulia : ";
+		cout << "SKS : " << sks;
+	}
+};
+
+int main()
+{
+	Mahasiswa mhs1;
+	Matakuliah mk;
+
+	cout << "Masukan NIM : ";
+	cin >> mhs1.nim;
+	cout << "Masukan Nama : ";
+	cin >> mhs1.nama;
+	mhs1.cetak();
+
+	mk.input();
+	mk.tampil();
+}
